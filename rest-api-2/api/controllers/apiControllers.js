ce3c86e4f109d130a1d3ced1f5eb8e26f02784f3
david.frigola <david.frigola@gmail.com>
2018-10-22 22:01:57 +0200

    Running local docker - draft approach

--- rest-api-2/api/controllers/apiControllers.js
+++ rest-api-2/api/controllers/apiControllers.js
@@ -4,7 +4,7 @@ exports.ping = function(req,res){
   res.send("app2")
 }
 exports.list = function(req,res){
-  request('http://localhost:3000/api/list', function (error, response, body) {
+  request('http://app1:3000/api/list', function (error, response, body) {
     if(body) {
       res.json(JSON.parse(body));
     }else{
@@ -14,7 +14,7 @@ exports.list = function(req,res){
 };
 
 exports.read = function(req,res){
-  request('http://localhost:3000/api/'+req.params.id, function (error, response, body) {
+  request('http://app1:3000/api/'+req.params.id, function (error, response, body) {
     res.status(response.statusCode)
     if(body) {
       res.json(JSON.parse(body));
