3290f2cc886c0fd5a71327c2579415cdd0305266
david.frigola <david.frigola@gmail.com>
2018-11-04 19:36:05 +0100

    #8 Mock strategy none for not using it

--- acceptance/features/support/env.rb
+++ acceptance/features/support/env.rb
@@ -4,21 +4,23 @@ require 'service_mock'
 @wiremock_server = ServiceMock::Server.new('standalone-2.19.0','../mock/wiremock')
 
 def start_wiremock(mappings_folder)
-  puts "Mock mappings folder : "+mappings_folder
-  @wiremock_server.start do |server|
-    server.port = 3000
-    server.verbose = true
-    server.root_dir = './'+mappings_folder
-  end
-  puts "Wiremock server started"
+  unless mappings_folder == 'none'
+    puts "Mock mappings folder : "+mappings_folder
+    @wiremock_server.start do |server|
+      server.port = 3000
+      server.verbose = true
+      server.root_dir = './'+mappings_folder
+    end
 
+    sleep 5 # FIXME Is there a better way to do this?
+    puts "Wiremock server started"
+  end
 end
 
-mock_strategy = ENV['strategy']||'simple'
+mock_strategy = ENV['strategy']||'none'
 
 start_wiremock(mock_strategy)
 
-sleep 5
 
 
 at_exit do
